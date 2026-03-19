e3163f958f8d519d7042f0139fae0746ad54d79a
sank173 <80623302+sank173@users.noreply.github.com>
2026-03-19 21:45:28 +0530

    Context-switch between the Process A and B without a Scheduler

--- kernel.c
+++ kernel.c
@@ -8,6 +8,49 @@ typedef uint32_t size_t;
 extern char __bss[], __bss_end[], __stack_top[];
 extern char __free_ram[], __free_ram_end[];
 
+
+__attribute__((naked)) void switch_context(uint32_t *prev_sp,
+                                           uint32_t *next_sp) {
+    __asm__ __volatile__(
+        // Save callee-saved registers onto the current process's stack.
+        "addi sp, sp, -13 * 4\n" // Allocate stack space for 13 4-byte registers
+        "sw ra,  0  * 4(sp)\n"   // Save callee-saved registers only
+        "sw s0,  1  * 4(sp)\n"
+        "sw s1,  2  * 4(sp)\n"
+        "sw s2,  3  * 4(sp)\n"
+        "sw s3,  4  * 4(sp)\n"
+        "sw s4,  5  * 4(sp)\n"
+        "sw s5,  6  * 4(sp)\n"
+        "sw s6,  7  * 4(sp)\n"
+        "sw s7,  8  * 4(sp)\n"
+        "sw s8,  9  * 4(sp)\n"
+        "sw s9,  10 * 4(sp)\n"
+        "sw s10, 11 * 4(sp)\n"
+        "sw s11, 12 * 4(sp)\n"
+
+        // Switch the stack pointer.
+        "sw sp, (a0)\n"         // *prev_sp = sp;
+        "lw sp, (a1)\n"         // Switch stack pointer (sp) here
+
+        // Restore callee-saved registers from the next process's stack.
+        "lw ra,  0  * 4(sp)\n"  // Restore callee-saved registers only
+        "lw s0,  1  * 4(sp)\n"
+        "lw s1,  2  * 4(sp)\n"
+        "lw s2,  3  * 4(sp)\n"
+        "lw s3,  4  * 4(sp)\n"
+        "lw s4,  5  * 4(sp)\n"
+        "lw s5,  6  * 4(sp)\n"
+        "lw s6,  7  * 4(sp)\n"
+        "lw s7,  8  * 4(sp)\n"
+        "lw s8,  9  * 4(sp)\n"
+        "lw s9,  10 * 4(sp)\n"
+        "lw s10, 11 * 4(sp)\n"
+        "lw s11, 12 * 4(sp)\n"
+        "addi sp, sp, 13 * 4\n"  // We've popped 13 4-byte registers from the stack
+        "ret\n"
+    );
+}
+
 paddr_t alloc_pages(uint32_t n){
     /* make the next_addr variable non changeable between the function calls , thus making it kind of global variable*/
     
@@ -145,20 +188,115 @@ void putchar(char ch)
 {
     sbi_ecall(ch, 0, 0, 0, 0, 0, 0, 1);
 }
+struct process procs[PROCS_MAX]; // All process control structures.
+
+struct process *create_process(uint32_t pc) {
+    // Find an unused process control structure.
+    struct process *proc = NULL;
+    int i;
+    for (i = 0; i < PROCS_MAX; i++) {
+        if (procs[i].state == PROC_UNUSED) {
+            proc = &procs[i];
+            break;
+        }
+    }
+
+    if (!proc)
+        PANIC("no free process slots");
+
+    // Stack callee-saved registers. These register values will be restored in
+    // the first context switch in switch_context.
+    uint32_t *sp = (uint32_t *) &proc->stack[sizeof(proc->stack)];
+    *--sp = 0;                      // s11
+    *--sp = 0;                      // s10
+    *--sp = 0;                      // s9
+    *--sp = 0;                      // s8
+    *--sp = 0;                      // s7
+    *--sp = 0;                      // s6
+    *--sp = 0;                      // s5
+    *--sp = 0;                      // s4
+    *--sp = 0;                      // s3
+    *--sp = 0;                      // s2
+    *--sp = 0;                      // s1
+    *--sp = 0;                      // s0
+    *--sp = (uint32_t) pc;          // ra
+
+    // Initialize fields.
+    proc->pid = i + 1;
+    proc->state = PROC_RUNNABLE;
+    proc->sp = (uint32_t) sp;
+    return proc;
+}
+
+void delay(void) {
+    for (int i = 0; i < 30000000; i++)
+        __asm__ __volatile__("nop"); // do nothing
+}
+
+
+
+struct process *proc_a;
+struct process *proc_b;
+struct process *current_proc;
+struct process *idle_proc;
+
+void yeild(void){
+    struct process * next = idle_proc;
+    for(int i =0;i<PROCS_MAX;i++){
+         struct process *proc = &procs[(current_proc->pid + i) %PROCS_MAX];
+         if(proc->state == PROC_RUNNABLE && proc->pid > 0){
+            next = proc;
+            break;
+         }
+         
+    }
+    if(next == current_proc){
+         return;
+    }
+
+    struct process *prev = current_proc;
+    current_proc = next;
+    switch_context(&prev->sp , &next->sp);
+}
+
+
+void proc_a_entry(void) {
+    printf("starting process A\n");
+    while (1) {
+        putchar('A');
+        // yeild();
+        switch_context(&proc_a->sp , &proc_b->sp);
+        delay(); 
+    }
+}
+
+void proc_b_entry(void) {
+    printf("starting process B\n");
+    while (1) {
+        putchar('B');
+        // yeild();
+        switch_context(&proc_b->sp , &proc_a->sp);
+        delay(); 
+    }
+}
 /**
  * llvm-addr2line -e kernel.elf 8020015e --> comannd line which tells at which line in the code the exception occured , use the sepc value
 */
 void kernel_main(void)
 {
-    /* intializing the bss section with default 0s*/
-    memset(__bss, 0, (size_t)__bss_end - (size_t)__bss);
+    memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
 
-    paddr_t paddr0 = alloc_pages(2);
-    paddr_t paddr1 = alloc_pages(1);
-    printf("alloc_pages test: paddr0=%x\n", paddr0);
-    printf("alloc_pages test: paddr1=%x\n", paddr1);
+    WRITE_CSR(stvec, (uint32_t) kernel_entry);
 
-    PANIC("booted!");
+    // idle_proc = create_process((uint32_t) NULL);
+    // idle_proc->pid = 0;
+    // current_proc = idle_proc;
+
+    proc_a = create_process((uint32_t) proc_a_entry);
+    proc_b = create_process((uint32_t) proc_b_entry);
+    // yeild();
+    proc_a_entry();
+    PANIC("unreachable here!");
    
 }
 
