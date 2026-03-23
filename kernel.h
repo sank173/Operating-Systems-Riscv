8ea427c6b0b537f18a7fc0edd76cd1628e9e9235
sank173 <80623302+sank173@users.noreply.github.com>
2026-03-23 23:16:24 +0530

    added Page Table

--- kernel.h
+++ kernel.h
@@ -5,10 +5,20 @@
 #define PROC_UNUSED   0   // Unused process control structure
 #define PROC_RUNNABLE 1   // Runnable process
 
+#define SATP_SV32 (1u << 31)
+#define PAGE_V (1 << 0)    // Valid bit , entry is enabled
+#define PAGE_R (1 << 1)    // Readable
+#define PAGE_W (1 << 2)    // Writable 
+#define PAGE_X (1 << 3)    // Exceutable
+#define PAGE_U (1 << 4)    // User(accessible in user mode)
+
+
+
 struct process {
     int pid;             // Process ID
     int state;           // Process state: PROC_UNUSED or PROC_RUNNABLE
     vaddr_t sp;          // Stack pointer
+    uint32_t *page_table;
     uint8_t stack[8192]; // Kernel stack
 };
 
--- run.sh
+++ run.sh
@@ -12,5 +12,11 @@ $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
     kernel.c common.c
 
 # Start QEMU
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
-    -kernel kernel.elf
+# $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+#     -kernel kernel.elf
+$QEMU -machine virt -bios default -nographic -serial stdio --no-reboot \
+    -kernel kernel.elf \
+    -monitor telnet:127.0.0.1:1234,server,nowait
+
+
+# nc 127.0.0.1 1234
