7be8c466a54b01f5eae9ac20f8d5dffaada3a23e
sank173 <80623302+sank173@users.noreply.github.com>
2026-03-23 23:16:24 +0530

    added Page Table

--- kernel.c
+++ kernel.c
@@ -7,6 +7,7 @@ typedef uint32_t size_t;
 
 extern char __bss[], __bss_end[], __stack_top[];
 extern char __free_ram[], __free_ram_end[];
+extern char __kernel_base[];
 
 
 __attribute__((naked)) void switch_context(uint32_t *prev_sp,
@@ -149,6 +150,27 @@ void kernel_entry(void){
 
     );
 }
+void map_page(uint32_t *table1 , uint32_t vaddr, paddr_t paddr , uint32_t flags){
+
+    if(!is_aligned(vaddr,PAGE_SIZE)){
+       PANIC("unaligned vaddr %x", vaddr);
+    }
+    if(!is_aligned(paddr,PAGE_SIZE)){
+       PANIC("unaligned paddr %x", paddr);
+    }
+
+    uint32_t vpn1 =  (vaddr >> 22) & 0x3ff;
+    if((table1[vpn1]  & PAGE_V) == 0){
+      uint32_t pt_paddr = alloc_pages(1);
+      table1[vpn1] = ((pt_paddr / PAGE_SIZE) << 10)  | PAGE_V;
+    }
+
+    uint32_t vpn0 = (vaddr >> 12) & 0x3ff;
+    uint32_t *table0 = (uint32_t *) ((table1[vpn1] >> 10) * PAGE_SIZE);
+    table0[vpn0] = ((paddr / PAGE_SIZE) << 10) | flags | PAGE_V;
+} 
+
+
 /*
 * In Riscv Exception is handled in this way 
 * The CPU checks for the medeleg register which decides which mode should handle the exception
@@ -224,10 +246,16 @@ struct process *create_process(uint32_t pc) {
     *--sp = 0;                      // s0
     *--sp = (uint32_t) pc;          // ra
 
+    uint32_t *page_table = (uint32_t *) alloc_pages(1);
+    for(paddr_t paddr = (paddr_t) __kernel_base ; paddr < (paddr_t) __free_ram_end ; paddr+=PAGE_SIZE)
+    map_page(page_table, paddr , paddr , PAGE_R | PAGE_W | PAGE_X);
+    
+
     // Initialize fields.
     proc->pid = i + 1;
     proc->state = PROC_RUNNABLE;
     proc->sp = (uint32_t) sp;
+    proc->page_table = page_table;
     return proc;
 }
 
@@ -258,9 +286,12 @@ void yeild(void){
     }
 
     __asm__ __volatile__(
+        "sfence.vma\n"
+        "csrw  satp , %[satp]\n"
+        "sfence.vma\n"
         "csrw sscratch , %[sscratch]\n"
         :
-        : [sscratch] "r" ((uint32_t) &next->stack[sizeof(next->stack)])
+        :[satp] "r" (SATP_SV32 | (uint32_t) next->page_table / PAGE_SIZE) , [sscratch] "r" ((uint32_t) &next->stack[sizeof(next->stack)])
     );
 
     struct process *prev = current_proc;
