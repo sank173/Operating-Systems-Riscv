678d1b5d635043770d24eb406621cc87ff5d00d3
sank173 <80623302+sank173@users.noreply.github.com>
2026-03-24 20:38:46 +0530

    enable user mode

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 set -xue
 
@@ -6,11 +7,18 @@ QEMU=qemu-system-riscv32
 # Path to clang and compiler flags
 CC=/opt/homebrew/opt/llvm/bin/clang  # Ubuntu users: use CC=clang
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fuse-ld=lld -fno-stack-protector -ffreestanding -nostdlib"
+OBJCOPY=/opt/homebrew/opt/llvm/bin/llvm-objcopy
 
-# Build the kernel
-$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
-    kernel.c common.c
+$CC $CFLAGS -Wl,-Tuser.ld -Wl,-Map=shell.map -o shell.elf shell.c user.c common.c
+$OBJCOPY --set-section-flags .bss=alloc,contents -O binary shell.elf shell.bin
+$OBJCOPY -Ibinary -Oelf32-littleriscv shell.bin shell.bin.o 
+
+$CC $CFLAGS -Wl -Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c common.c shell.bin.o
 
 # Start QEMU
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
     -kernel kernel.elf
+
+
+
+
---
+++ shell.c
@@ -0,0 +1,7 @@
+#include "user.h"
+
+void main(void){
+      *((volatile int *) 0x80200000) = 0x1234; // new!
+    //  for(;;);
+
+}
